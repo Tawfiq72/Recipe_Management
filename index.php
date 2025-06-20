b82cf6df35eb507058dd4a978b09fea7d1a6a662
Tawfiq72 <tawfiq1921@gmail.com>
2025-06-20 18:24:34 +0600

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include 'views/includes/header.php';
+?>
+<h1>Welcome to Recipe App</h1>
+<p>Explore our features:</p>
+<ul>
+    <li><a href="views/recipe.php">Recipe Gallery</a></li>
+    <li><a href="views/timer.php">Cooking Timer</a></li>
+</ul>
+<?php
+include 'views/includes/footer.php';
+?>
