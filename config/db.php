d8b5b0e81b0433aabbd647b9a08507c324bf5c13
Tawfiq72 <tawfiq1921@gmail.com>
2025-06-25 00:03:36 +0600

    updated database connection

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+$host = 'localhost';
+$dbname = 'recipe_db';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new mysqli($host, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    die("Database connection error: " . $e->getMessage());
+}
+?>
