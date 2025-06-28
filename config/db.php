74b409f20e5f8422ffc1efe412acb92c0b68cc3d
Lisonnnnn <sabbirhasan22k@gmail.com>
2025-06-28 12:51:18 +0600

    simplify database connection

--- config/db.php
+++ config/db.php
@@ -1,16 +1,14 @@
 <?php
 // Database configuration
-$host = 'localhost';
-$dbname = 'recipe_db';
-$username = 'root';
-$password = '';
+$host='localhost';
+$dbname='recipe_db';
+$username='root';
+$password='';
 
-try {
+
     $conn = new mysqli($host, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
+    if($conn->connect_error){
+        die("connection failed:".$conn->connect_error);
+
     }
-} catch (Exception $e) {
-    die("Database connection error: " . $e->getMessage());
-}
 ?>
